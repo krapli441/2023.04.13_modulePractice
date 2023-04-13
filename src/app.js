5bf2804a531071b519e553bfa58be55ce351c984
krapli441 <tlfqj1208@gmail.com>
2023-04-13 21:58:09 +0900

    node 오류

---
+++ src/app.js
@@ -0,0 +1,18 @@
+import http from "http";
+import fs from "fs";
+
+const server = http.createServer(function (request, response) {
+  if (request.method === "GET") {
+    const firstPage = fs.readFileSync("./public/index.html");
+    response.writeHead(200, { "Content-Type": "text/html" });
+    response.end(firstPage);
+  }
+});
+
+server.listen(2080, function (error) {
+  if (error) {
+    console.log(error);
+    throw error;
+  }
+  console.log("server is running");
+});
