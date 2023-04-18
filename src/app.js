8d664e45b1126c9a38ba0c88460252c81ab10f17
박준형 <108733795+krapli441@users.noreply.github.com>
2023-04-18 18:02:19 +0900

    귀가 전 커밋

--- src/app.js
+++ src/app.js
@@ -1,13 +1,16 @@
 import http from "http";
 import fs from "fs";
+import path from "path";
 
 import append from "../utility/append.js";
 import divide from "../utility/divide.js";
 import multiply from "../utility/multiply.js";
 import subtract from "../utility/subtract.js";
 
-const functionList = fs.readdirSync("../utility/");
+const functionList = "../utility/";
+const files = fs.readdirSync(functionList);
 console.log(functionList.length);
+console.log(files);
 
 const server = http.createServer(function (request, response) {
   if (request.method === "GET") {
@@ -30,8 +33,8 @@ server.listen(2080, function (error) {
   }
   console.log("server is running");
 
-  console.log(append(10, 20));
-  console.log(subtract(50, 25));
-  console.log(multiply(10, 5));
-  console.log(divide(100, 5));
+  // console.log(append(10, 20));
+  // console.log(subtract(50, 25));
+  // console.log(multiply(10, 5));
+  // console.log(divide(100, 5));
 });
